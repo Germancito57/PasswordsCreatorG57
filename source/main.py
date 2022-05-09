ee4bc90325d45225fedb6df352bd8da6e56272a7
German Fernandez <germanfernandez0306@gmail.com>
2022-05-09 19:25:13 -0300

    test

--- source/main.py
+++ source/main.py
@@ -1,1 +1,17 @@
-print("d")
+import tkinter
+
+window = tkinter.Tk()
+
+button = tkinter.Button(window, text="Click Me!")
+button.pack()
+
+button2 = tkinter.Button(window, text="Click 222!")
+button2.pack()
+
+
+
+
+list = tkinter.Listbox(window)
+list.pack()
+
+window.mainloop()
